96ac7c5f23ab9427485626ccac05e9e10f12dbb7
Irving Zamora <irvingzamora28@gmail.com>
2024-11-28 04:15:58 -0700

    chore: Improve data integrity by ensuring user association deletion in game players table
    Update user_id foreign key constraint to cascade on delete for seamless data consistency

--- database/migrations/2024_03_19_000004_create_game_players_table.php
+++ database/migrations/2024_03_19_000004_create_game_players_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('game_players', function (Blueprint $table) {
             $table->id();
             $table->foreignId('game_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('user_id')->nullable()->constrained();
+            $table->foreignId('user_id')->nullable()->constrained()->cascadeOnDelete();
             $table->uuid('guest_id')->nullable();
             $table->string('player_name');
             $table->integer('score')->default(0);
