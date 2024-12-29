33146c7c819fee3d228080ad51809c7e1ee83b9b
Irving Zamora <irvingzamora28@gmail.com>
2024-12-29 04:47:03 -0700

    feat: Add general translations for English, Spanish, and German in language files

--- resources/js/Components/GuestLanguageModal.tsx
+++ resources/js/Components/GuestLanguageModal.tsx
@@ -117,7 +117,7 @@ export default function GuestLanguageModal({ show, onClose, languagePairs }: Pro
                                     whileTap="tap"
                                     className="px-4 py-2 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200 transition-colors"
                                 >
-                                    {t('cancel')}
+                                    {t('generals.cancel')}
                                 </motion.button>
                                 <motion.button
                                     type="submit"
