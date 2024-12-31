a1955356d1ae7107b9734756f039510bfed64ac5
Irving Zamora <irvingzamora28@gmail.com>
2024-12-31 14:58:09 -0700

    feat: Add NounService for retrieving nouns with translation and category filtering

---
+++ app/Services/NounService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Noun;
+
+class NounService {
+
+    public function getNouns(int $language_id, int $translation_language_id, int $category_id, int $totalRounds): array {
+        $query = Noun::where('language_id', $language_id);
+        if ($category_id !== 0) {
+            $query->whereHas('categories', function ($query) use ($category_id) {
+                $query->where('category_id', $category_id);
+            });
+        }
+        return $query->inRandomOrder()
+            ->limit($totalRounds)
+            ->get()
+            ->map(fn($noun) => [
+                'id' => $noun->id,
+                'word' => $noun->word,
+                'gender' => $noun->gender,
+                'translation' => $noun->getTranslation($translation_language_id),
+            ])
+            ->toArray();
+    }
+}
