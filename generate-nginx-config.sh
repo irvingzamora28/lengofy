209238b0ae2b0421d92e73772b78b4d9cc5a6978
Irving Zamora <irvingzamora28@gmail.com>
2025-01-13 05:54:33 -0700

    fix(ci): Refactors nginx config generation for known relative path

--- generate-nginx-config.sh
+++ generate-nginx-config.sh
@@ -3,5 +3,8 @@
 # Get the directory of the script
 SCRIPT_DIR="$(dirname "$0")"
 
-# Use the correct path to the template file
-envsubst '${SERVER_NAME}' < "$SCRIPT_DIR/../nginx/default.conf.template" > "$SCRIPT_DIR/../nginx/default.conf"
+# Print the directory for debugging
+echo "Script directory: $SCRIPT_DIR"
+
+# Use the known relative path to the template file from the project root
+envsubst '${SERVER_NAME}' < "$SCRIPT_DIR/nginx/default.conf.template" > "$SCRIPT_DIR/nginx/default.conf"
