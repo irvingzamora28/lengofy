2ca2688614c7e0fcf0c3d0ea295823cd6e8db80c
Irving Zamora <irvingzamora28@gmail.com>
2025-01-13 06:02:59 -0700

    fix(ci): Correct nginx config generation in deployment workflow
    - Introduce generation of nginx config with production server name
    - Improve deployment process by ensuring correct nginx configuration

--- generate-nginx-config.sh
+++ generate-nginx-config.sh
@@ -6,5 +6,11 @@ SCRIPT_DIR="$(dirname "$0")"
 # Print the directory for debugging
 echo "Script directory: $SCRIPT_DIR"
 
+# Check if SERVER_NAME is provided
+if [ -z "$1" ]; then
+  echo "No SERVER_NAME provided. Exiting."
+  exit 1
+fi
+
 # Use the known relative path to the template file from the project root
 envsubst '${SERVER_NAME}' < "$SCRIPT_DIR/nginx/default.conf.template" > "$SCRIPT_DIR/nginx/default.conf"
