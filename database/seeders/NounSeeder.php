8906354a61abf2c2ace277a81274eb0cdf2dd6c1
Irving Zamora <irvingzamora28@gmail.com>
2025-01-14 13:04:44 -0700

    feat: Refactor NounSeeder to load multiple noun seed files and optimize database insertion - Create seed for nouns of the following categories: animals food_drinks transportation technology nature clothing furniture sports household body school professions people_family health weather time numbers colors shapes miscellaneous

--- database/seeders/NounSeeder.php
+++ database/seeders/NounSeeder.php
@@ -12,33 +12,69 @@ class NounSeeder extends Seeder
 {
     public function run(): void
     {
-        $nouns = json_decode(file_get_contents(database_path('seeds/nouns_seed.json')), true);
+        $files = [
+            'animals_nouns_seed.json',
+            'colors_nouns_seed.json',
+            'health_nouns_seed.json',
+            'nature_nouns_seed.json',
+            'people_family_nouns_seed.json',
+            'shapes_nouns_seed.json',
+            'time_nouns_seed.json',
+            'body_nouns_seed.json',
+            'food_drinks_nouns_seed.json',
+            'household_nouns_seed.json',
+            'professions_nouns_seed.json',
+            'sports_nouns_seed.json',
+            'transportation_nouns_seed.json',
+            'clothing_nouns_seed.json',
+            'furniture_nouns_seed.json',
+            'miscellaneous_nouns_seed.json',
+            'numbers_nouns_seed.json',
+            'school_nouns_seed.json',
+            'technology_nouns_seed.json',
+            'weather_nouns_seed.json',
+        ];
+
+        $nouns = [];
+        foreach ($files as $file) {
+            $filePath = database_path('seeds/' . $file);
+            $fileNouns = json_decode(file_get_contents($filePath), true);
+            $nouns = array_merge($nouns, $fileNouns);
+        }
         $language = Language::where('name', 'Deutsch')->firstOrFail();
 
+        $nounData = [];
+        $categoryNounData = [];
+        $translationData = [];
+
         foreach ($nouns as $noun) {
+        $category = Category::where('key', $noun['category'])->firstOrFail();
 
-            $category = Category::where('key', $noun['category'])->firstOrFail();
+        $newNoun = Noun::create([
+            'word' => $noun['word'],
+            'gender' => $noun['gender'],
+            'difficulty_level' => $noun['difficulty_level'],
+            'emoji' => $noun['emoji'] ?? null,
+            'language_id' => $language->id,
+        ]);
 
-            $newNoun = new Noun();
-            $newNoun->word = $noun['word'];
-            $newNoun->gender = $noun['gender'];
-            $newNoun->difficulty_level = $noun['difficulty_level'];
-            $newNoun->language()->associate($language);
-            $newNoun->save();
+        $categoryNounData[] = [
+            'category_id' => $category->id,
+            'noun_id' => $newNoun->id,
+        ];
+        $translationLanguages = Language::whereIn('code', array_keys($noun['translations']))->get()->keyBy('code');
 
-            $newNoun->categories()->attach($category->id);
-
-            // Associate english translation
-            $translationLanguages = Language::whereIn('code', array_keys($noun['translations']))->get()->keyBy('code');
-
-            foreach ($noun['translations'] as $languageCode => $translation) {
-                $translationLanguage = $translationLanguages[$languageCode];
-                DB::table('noun_translations')->insert([
-                    'language_id' => $translationLanguage->id,
-                    'translation' => $translation,
-                    'noun_id' => $newNoun->id
-                ]);
-            }
+        foreach ($noun['translations'] as $languageCode => $translation) {
+            $translationLanguage = $translationLanguages[$languageCode];
+            $translationData[] = [
+                'language_id' => $translationLanguage->id,
+                'translation' => $translation,
+                'noun_id' => $newNoun->id,
+            ];
         }
     }
-}
+
+    DB::table('nouns')->insert($nounData);
+    DB::table('category_noun')->insert($categoryNounData);
+    DB::table('noun_translations')->insert($translationData);
+}}
