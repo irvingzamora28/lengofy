2e9f05a41c8d31b888a078a7f3dbf50eb2592a9e
Irving Zamora <irvingzamora28@gmail.com>
2025-02-10 18:54:16 -0700

    style: Add margin to TipBox component for improved spacing

--- resources/js/Components/Lessons/TipBox.tsx
+++ resources/js/Components/Lessons/TipBox.tsx
@@ -8,7 +8,7 @@ interface TipBoxProps {
 
 const TipBox: React.FC<TipBoxProps> = ({ children }) => {
     return (
-        <div className="group transition-all duration-300 bg-gradient-to-r from-blue-50 to-blue-100 hover:from-blue-100 hover:to-blue-200 border-l-4 border-blue-500 rounded-lg px-4 py-3 shadow-sm hover:shadow-md dark:from-indigo-950 dark:to-indigo-900 dark:hover:from-indigo-900 dark:hover:to-indigo-800 dark:text-indigo-50 dark:border-indigo-400" role="alert">
+        <div className="group transition-all my-4 duration-300 bg-gradient-to-r from-blue-50 to-blue-100 hover:from-blue-100 hover:to-blue-200 border-l-4 border-blue-500 rounded-lg px-4 py-3 shadow-sm hover:shadow-md dark:from-indigo-950 dark:to-indigo-900 dark:hover:from-indigo-900 dark:hover:to-indigo-800 dark:text-indigo-50 dark:border-indigo-400" role="alert">
             <div className="flex items-start gap-3 sm:gap-4">
                 <div className="flex-shrink-0 mt-1">
                     <IoIosBulb className="h-5 w-5 sm:h-6 sm:w-6 text-blue-500 dark:text-indigo-300 group-hover:text-blue-600 dark:group-hover:text-indigo-200 transition-colors duration-300" />
